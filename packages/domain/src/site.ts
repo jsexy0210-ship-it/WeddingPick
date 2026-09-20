e0e1c5cf58bc62043d618c505a067a066ad3ec1b
재또 <jsexy0210@gmail.com>
2026-09-20 12:19:13 +0900

    docs(domain): remove obsolete hosting note

--- packages/domain/src/site.ts
+++ packages/domain/src/site.ts
@@ -1,7 +1,7 @@
 /**
  * 서비스 웹사이트 주소.
  *
- * Render는 2026-09-18부터 폐기 경로다. 공개 웹사이트·약관 정본은 KakaoCloud VM의
+ * 공개 웹사이트·약관 정본은 KakaoCloud VM의
  * 별도 HTTPS origin을 사용한다. 관리자와 사용자 앱도 서로 다른 origin을 유지한다.
  */
 export const SITE_ORIGIN = 'https://210.109.82.212:9443';
